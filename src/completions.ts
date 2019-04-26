48537484e82e849389eb53d2f6f800f51e191205
Fredrik Lengstrand <10126466+fredriklengstrand@users.noreply.github.com>
2019-04-26 11:49:46 +0200

    Add completion provider for methods

--- src/completions.ts
+++ src/completions.ts
@@ -1,56 +1,106 @@
-import { CompletionItemProvider, CancellationToken, TextDocument, Position, CompletionContext, CompletionItem, CompletionItemKind, SnippetString, MarkdownString } from 'vscode';
+import { CompletionItemProvider, CancellationToken, TextDocument, Position, CompletionContext, CompletionItem, CompletionItemKind, SnippetString, MarkdownString, Range } from 'vscode';
 import docs from './docs';
 
 export default class Completions implements CompletionItemProvider
 {
+  triggerCharacters: string[];
+
+  constructor(...triggerCharacters: string[])
+  {
+    this.triggerCharacters = triggerCharacters;
+  }
+
   public provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken, context: CompletionContext): CompletionItem[] {
     let completionItems: CompletionItem[] = [];
   
     const { classes, functions, variables, keywords } = docs(document);
 
-    for (let item of classes) {
-      if (typeof item.compat === 'undefined' && (typeof item.deprecated === 'undefined' || item.deprecated === false)) {
-        if (item.name === 'MailMessage' || item.name === 'MIMEPart' || item.name === 'LDAPResult') continue;
-        let completionItem = new CompletionItem(item.name, CompletionItemKind.Class);
-        completionItem.detail = item.detail;
-        completionItem.documentation = new MarkdownString(item.documentation);
-        completionItem.insertText = new SnippetString(item.value);
-        completionItems.push(completionItem);
+    if (!this.triggerCharacters.length) {
+      let isMethod = false;
+      const wordRange = document.getWordRangeAtPosition(position);
+      if (wordRange !== undefined) {
+        const text = document.getText(wordRange);
+        if (text) {
+          isMethod = document.getText(new Range(position.line, wordRange.start.character >= 2 ? wordRange.start.character -2 : 0, position.line, wordRange.start.character)) === '->';
+        }
       }
-    }
-
-    for (let item of functions) {
-      if (typeof item.compat === 'undefined' && (typeof item.deprecated === 'undefined' || item.deprecated === false)) {
-        let completionItem = new CompletionItem(item.name, CompletionItemKind.Function);
-        completionItem.detail = item.detail;
-        completionItem.documentation = new MarkdownString(item.documentation);
-        completionItem.insertText = new SnippetString(item.value);
-        completionItems.push(completionItem);
-      }
-    }
-
-    for (let item of variables) {
-      if (typeof item.compat === 'undefined' && (typeof item.deprecated === 'undefined' || item.deprecated === false)) {
-        let completionItem = new CompletionItem(item.name, CompletionItemKind.Variable);
-        completionItem.detail = item.detail;
-        completionItem.documentation = new MarkdownString(item.example ? `${item.documentation} \n\n Example: \`${item.example}\`` : item.documentation);
-        completionItems.push(completionItem);
-      }
-    }
-
-    for (let item of keywords) {
-      let completionItem = new CompletionItem(item.name, CompletionItemKind.Keyword);
-      completionItem.detail = item.detail;
-      completionItem.documentation = new MarkdownString(item.documentation);
-      completionItem.insertText = new SnippetString(item.value || item.name);
-      completionItems.push(completionItem);
-      if (typeof item.snippets !== 'undefined') {
-        for (let snippet of item.snippets) {
-          let completionItem = new CompletionItem(item.name, CompletionItemKind.Snippet);
-          completionItem.detail = snippet.detail;
-          completionItem.documentation = new MarkdownString(snippet.documentation || item.documentation);
-          completionItem.insertText = new SnippetString(snippet.insertText.join('\n'));
+      if (isMethod) {
+        for (let item of classes) {
+          if (typeof item.compat === 'undefined' && (typeof item.deprecated === 'undefined' || item.deprecated === false)) {
+            for (let method of item.methods) {
+              let completionItem = new CompletionItem(`(${item.name}) ${method.name}`, CompletionItemKind.Method);
+              completionItem.detail = method.detail;
+              completionItem.documentation = new MarkdownString(method.documentation);
+              completionItem.insertText = new SnippetString(method.value);
+              completionItems.push(completionItem);
+            }
+          }
+        }
+      } else {
+        for (let item of classes) {
+          if (typeof item.compat === 'undefined' && (typeof item.deprecated === 'undefined' || item.deprecated === false)) {
+            if (item.name === 'MailMessage' || item.name === 'MIMEPart' || item.name === 'LDAPResult') continue;
+            let completionItem = new CompletionItem(item.name, CompletionItemKind.Class);
+            completionItem.detail = item.detail;
+            completionItem.documentation = new MarkdownString(item.documentation);
+            completionItem.insertText = new SnippetString(item.value);
+            completionItems.push(completionItem);
+          }
+        }
+    
+        for (let item of functions) {
+          if (typeof item.compat === 'undefined' && (typeof item.deprecated === 'undefined' || item.deprecated === false)) {
+            let completionItem = new CompletionItem(item.name, CompletionItemKind.Function);
+            completionItem.detail = item.detail;
+            completionItem.documentation = new MarkdownString(item.documentation);
+            completionItem.insertText = new SnippetString(item.value);
+            completionItems.push(completionItem);
+          }
+        }
+    
+        for (let item of variables) {
+          if (typeof item.compat === 'undefined' && (typeof item.deprecated === 'undefined' || item.deprecated === false)) {
+            let completionItem = new CompletionItem(item.name, CompletionItemKind.Variable);
+            completionItem.detail = item.detail;
+            completionItem.documentation = new MarkdownString(item.example ? `${item.documentation} \n\n Example: \`${item.example}\`` : item.documentation);
+            completionItems.push(completionItem);
+          }
+        }
+    
+        for (let item of keywords) {
+          let completionItem = new CompletionItem(item.name, CompletionItemKind.Keyword);
+          completionItem.detail = item.detail;
+          completionItem.documentation = new MarkdownString(item.documentation);
+          completionItem.insertText = new SnippetString(item.value || item.name);
           completionItems.push(completionItem);
+          if (typeof item.snippets !== 'undefined') {
+            for (let snippet of item.snippets) {
+              let completionItem = new CompletionItem(item.name, CompletionItemKind.Snippet);
+              completionItem.detail = snippet.detail;
+              completionItem.documentation = new MarkdownString(snippet.documentation || item.documentation);
+              completionItem.insertText = new SnippetString(snippet.insertText.join('\n'));
+              completionItems.push(completionItem);
+            }
+          }
+        }
+      }
+    }
+
+    for (let triggerCharacter of this.triggerCharacters) {
+      if (triggerCharacter === '>') {
+        let isMethod = document.getText(new Range(position.line, position.character >= 2 ? position.character -2 : 0, position.line, position.character)) === '->';
+        if (isMethod) {
+          for (let item of classes) {
+            if (typeof item.compat === 'undefined' && (typeof item.deprecated === 'undefined' || item.deprecated === false)) {
+              for (let method of item.methods) {
+                let completionItem = new CompletionItem(`(${item.name}) ${method.name}`, CompletionItemKind.Method);
+                completionItem.detail = method.detail;
+                completionItem.documentation = new MarkdownString(method.documentation);
+                completionItem.insertText = new SnippetString(method.value);
+                completionItems.push(completionItem);
+              }
+            }
+          }
         }
       }
     }
--- src/extension.ts
+++ src/extension.ts
@@ -26,6 +26,7 @@ export function activate(context: ExtensionContext)
 
   context.subscriptions.push(languages.registerDocumentLinkProvider('hsl', new Links()));
   context.subscriptions.push(languages.registerCompletionItemProvider('hsl', new Completions()));
+  context.subscriptions.push(languages.registerCompletionItemProvider('hsl', new Completions('>'), '>'));
   context.subscriptions.push(languages.registerHoverProvider('hsl', new Hovers()));
 
   context.subscriptions.push(languages.setLanguageConfiguration('hsl', {
