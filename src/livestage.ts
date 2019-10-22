f1cfa6a37109966c09aad3518c13e64480d5328b
Fredrik Lengstrand <10126466+fredriklengstrand@users.noreply.github.com>
2019-10-22 15:02:35 +0200

    Merge smtpd and queued

--- src/livestage.ts
+++ src/livestage.ts
@@ -8,13 +8,12 @@ export default (connector: factory.SSH2Connector | factory.UNIXConnector, worksp
   if (command === 'start')
   {
     build.run(workspacePath);
-    const smtpdConfig = fs.readFileSync(path.join(workspacePath, "dist", "smtpd-app.yaml")).toString();
-    const queuedConfig = fs.readFileSync(path.join(workspacePath, "dist", "queued-app.yaml")).toString();
+    const config = fs.readFileSync(path.join(workspacePath, "dist", "smtpd-app.yaml")).toString();
     const userSettings = JSON.parse(fs.readFileSync(path.join(workspacePath, "settings.json")).toString());
     const conditions = userSettings.livestage && userSettings.livestage.conditions ? userSettings.livestage.conditions : {};
     const id = userSettings.livestage && userSettings.livestage.id ? userSettings.livestage.id : "abcd";
 
-    remote.startLiveStage(connector, id, conditions, smtpdConfig, queuedConfig).then(() => {
+    remote.startLiveStage(connector, id, conditions, config).then(() => {
       window.showInformationMessage('Live Staging: Started');
     }).catch(error => {
       window.showWarningMessage(`Live Staging: ${error.message || error}`);
