860bb251ba18b5c6770e90e08b4f88423e770a2c
Fredrik Lengstrand <10126466+fredriklengstrand@users.noreply.github.com>
2021-06-05 10:40:16 +0000

    Add configuration provider

--- debug/src/extension.ts
+++ debug/src/extension.ts
@@ -39,7 +39,29 @@ export function activate(context: vscode.ExtensionContext) {
       return new vscode.DebugAdapterInlineImplementation(new HSLLoggingDebugSession());
     }
   }));
+
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('hsl', new HSLConfigurationProvider()));
 }
 
 export function deactivate() {
+}
+
+class HSLConfigurationProvider implements vscode.DebugConfigurationProvider {
+	resolveDebugConfiguration(folder: vscode.WorkspaceFolder | undefined, config: vscode.DebugConfiguration, token?: vscode.CancellationToken): vscode.ProviderResult<vscode.DebugConfiguration> {
+		if (!config.type && !config.request && !config.name) {
+			const editor = vscode.window.activeTextEditor;
+			if (editor && editor.document.languageId === 'hsl') {
+				config.name = 'Debug File';
+        config.type = 'hsl';
+				config.request = 'launch';
+				config.program = '${file}';
+        config.debug = true;
+			}
+		}
+		if (!config.program) {
+			vscode.window.showInformationMessage('Cannot find a program to debug');
+      return undefined;
+		}
+		return config;
+	}
 }
