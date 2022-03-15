b3dd7f3fa833bd380defcd5ad2d66bcfd4a6989c
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2022-03-15 13:06:57 +0100

    Update extensions in dev container

--- config/src/init.ts
+++ config/src/init.ts
@@ -25,7 +25,7 @@ export const run = (base: string | null = '.', template = 'minimal', development
       forwardPorts: [25],
       // appPort: [25],
       overrideCommand: false,
-      extensions: ['Halon.vscode-halon', 'Halon.hsl-linter', 'Halon.hsl-debug'],
+      extensions: ['Halon.vscode-halon', 'Halon.hsl-syntax', 'Halon.hsl-linter', 'Halon.hsl-debug'],
       mounts: [
         'source=${localWorkspaceFolder}/dist,target=/etc/halon,type=bind,consistency=cached'
       ],
