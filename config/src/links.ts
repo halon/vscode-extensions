35c97058e962c97b43c13779c608b65a1307b30e
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2022-10-03 16:25:28 +0200

    Add support for file paths in links

--- config/src/links.ts
+++ config/src/links.ts
@@ -1,5 +1,7 @@
 import { DocumentLinkProvider, CancellationToken, DocumentLink, TextDocument, Range, Uri, workspace } from 'vscode';
+import * as fs from 'fs';
 import * as path from 'path';
+import yaml from 'yaml';
 
 export default class Links implements DocumentLinkProvider
 {
@@ -10,25 +12,49 @@ export default class Links implements DocumentLinkProvider
     if (typeof workspaceFolder !== 'undefined') {
       const workspacePath = workspaceFolder.uri.fsPath;
 
+      let rootPath: string | null = null;
+      let smtpdPath = path.join(workspacePath, 'src', 'config', 'smtpd.yaml');
+      if (!fs.existsSync(smtpdPath)) {
+        smtpdPath = path.join('etc', 'halon', 'smtpd.yaml');
+      }
+      if (fs.existsSync(smtpdPath)) {
+        try {
+          const smtpd = yaml.parse(fs.readFileSync(smtpdPath).toString());
+          if (smtpd.scripting !== undefined && smtpd.scripting.rootpath !== undefined) {
+            rootPath = smtpd.scripting.rootpath;
+          }
+        } catch (err) {}
+      }
+
       let patterns: RegExp[] = [];
-      patterns.push(/(.*?(?:from)\s+\"(?:.*!)?)((([^."]+)([\.\/][^."]+)*))\"/mg);
-      patterns.push(/(.*?(?:include|include_once)\s+\")((?:[^."]+)(?:[\.\/][^."]+)*)\"/mg);
+      patterns.push(/(.*?(?:from)\s+\"(?:.*!)?)(file:\/\/)?((([^."]+)([\.\/][^."]+)*))\"/mg);
+      patterns.push(/(.*?(?:include|include_once)\s+\")(file:\/\/)?((?:[^."]+)(?:[\.\/][^."]+)*)\"/mg);
 
       const text  = document.getText();
 
       for (let pattern of patterns) {
         let match: RegExpExecArray | null = null;
-        while ((match = pattern.exec(text)) !== null && !match[2].includes('*')) {
+        while ((match = pattern.exec(text)) !== null && !match[3].includes('*')) {
           const pre = match[1];
-          const link = match[2];
+          const type = match[2] ?? '';
+          const link = match[3].split(path.posix.sep).join(path.sep);
           const offset = (match.index || 0) + pre.length;
           const start = document.positionAt(offset);
-          const end = document.positionAt(offset + link.length);
+          const end = document.positionAt(offset + type.length + link.length);
           const range = new Range(start, end);
-          const uri = path.join(workspacePath, 'src', 'files', link);
-          const target = Uri.file(uri);
-          const documentLink = new DocumentLink(range, target);
-          links.push(documentLink);
+          let uri: string | null = null;
+          if (type === 'file://') {
+            if (rootPath !== null) {
+              uri = path.join(rootPath, link);
+            }
+          } else {
+            uri = path.join(workspacePath, 'src', 'files', link);
+          }
+          if (uri) {
+            const target = Uri.file(uri);
+            const documentLink = new DocumentLink(range, target);
+            links.push(documentLink);
+          }
         }
       }
     }
