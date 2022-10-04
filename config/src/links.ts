6a01338ba840e7ee0924fe91c580b2db5fa05821
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2022-10-04 11:29:27 +0200

    Support relative paths and index files in document links

--- config/src/links.ts
+++ config/src/links.ts
@@ -11,24 +11,12 @@ export default class Links implements DocumentLinkProvider
     const workspaceFolder = workspace.getWorkspaceFolder(document.uri);
     if (typeof workspaceFolder !== 'undefined') {
       const workspacePath = workspaceFolder.uri.fsPath;
-
+      const relativePath = path.dirname(document.uri.fsPath);
       let rootPath: string | null = null;
-      let smtpdPath = path.join(workspacePath, 'src', 'config', 'smtpd.yaml');
-      if (!fs.existsSync(smtpdPath)) {
-        smtpdPath = path.join('etc', 'halon', 'smtpd.yaml');
-      }
-      if (fs.existsSync(smtpdPath)) {
-        try {
-          const smtpd = yaml.parse(fs.readFileSync(smtpdPath).toString());
-          if (smtpd.scripting !== undefined && smtpd.scripting.rootpath !== undefined) {
-            rootPath = smtpd.scripting.rootpath;
-          }
-        } catch (err) {}
-      }
 
       let patterns: RegExp[] = [];
-      patterns.push(/(.*?(?:from)\s+\"(?:.*!)?)(file:\/\/)?((([^."]+)([\.\/][^."]+)*))\"/mg);
-      patterns.push(/(.*?(?:include|include_once)\s+\")(file:\/\/)?((?:[^."]+)(?:[\.\/][^."]+)*)\"/mg);
+      patterns.push(/(.*?(?:from)\s+\"(?:.*!)?)(file:\/\/)?((?:.\/)?(([^."]+)([\.\/][^."]+)*))\"/mg);
+      patterns.push(/(.*?(?:include|include_once)\s+\")(file:\/\/)?((?:.\/)?(?:[^."]+)(?:[\.\/][^."]+)*)\"/mg);
 
       const text  = document.getText();
 
@@ -38,18 +26,49 @@ export default class Links implements DocumentLinkProvider
           const pre = match[1];
           const type = match[2] ?? '';
           const link = match[3].split(path.posix.sep).join(path.sep);
+          const extension = match[6];
           const offset = (match.index || 0) + pre.length;
           const start = document.positionAt(offset);
           const end = document.positionAt(offset + type.length + link.length);
           const range = new Range(start, end);
+          const relative = link.substring(0, 2) === './';
+
           let uri: string | null = null;
-          if (type === 'file://') {
-            if (rootPath !== null) {
-              uri = path.join(rootPath, link);
-            }
+          if (relative) {
+            // Relative files
+            uri = path.join(relativePath, link);
           } else {
-            uri = path.join(workspacePath, 'src', 'files', link);
+            if (type === 'file://') {
+              // External files
+              if (rootPath === null) {
+                let smtpdPath = path.join(workspacePath, 'src', 'config', 'smtpd.yaml');
+                if (!fs.existsSync(smtpdPath)) {
+                  smtpdPath = path.join('etc', 'halon', 'smtpd.yaml');
+                }
+                if (fs.existsSync(smtpdPath)) {
+                  try {
+                    const smtpd = yaml.parse(fs.readFileSync(smtpdPath).toString());
+                    if (smtpd.scripting !== undefined && smtpd.scripting.rootpath !== undefined) {
+                      rootPath = smtpd.scripting.rootpath;
+                    }
+                  } catch (err) {}
+                }
+              }
+              if (rootPath !== null) {
+                uri = path.join(rootPath, link);
+              }
+            } else {
+              // Workspace files
+              uri = path.join(workspacePath, 'src', 'files', link);
+            }
           }
+
+          // Index files
+          if (uri && !extension && fs.existsSync(path.join(uri, 'index.hsl'))) {
+            uri = path.join(uri, 'index.hsl');
+          }
+
+          // Append link
           if (uri) {
             const target = Uri.file(uri);
             const documentLink = new DocumentLink(range, target);
