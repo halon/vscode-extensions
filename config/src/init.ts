1f6b15b24ab4b73ee8e35aef5b548bd4c71b3fc0
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2023-04-11 13:37:58 +0200

    Bump version

--- config/src/init.ts
+++ config/src/init.ts
@@ -56,7 +56,7 @@ RUN apt-get install -y apt-transport-https
 RUN wget -qO - https://raw.githubusercontent.com/halon/changelog/master/pgp-keys/7F0A73B5.asc | apt-key add -
 RUN echo "deb https://repo.halon.io/ jammy stable" >> /etc/apt/sources.list.d/halon.list
 RUN echo "machine repo.halon.io login \${HALON_REPO_USER} password \${HALON_REPO_PASS}" >> /etc/apt/auth.conf
-RUN apt-get update && apt-get install -y halon=5.11.0 halon-rated=5.11.0 halon-dlpd=5.11.0 halon-extras-rate=1.1.0 halon-extras-dlp=1.1.0
+RUN apt-get update && apt-get install -y halon=5.12.0 halon-rated=5.12.0 halon-dlpd=5.12.0 halon-extras-rate=1.1.0 halon-extras-dlp=1.1.0
 
 RUN /usr/bin/install -d /var/run/halon
 ENV LD_LIBRARY_PATH=$LD_LIBRARY_PATH
@@ -149,7 +149,7 @@ Accept();
   fs.writeFileSync(path.join(base, "src", "hooks", "eod", "default.hsl"), eod_default);
 
   const smtpd_app: any = {
-    version: '5.11',
+    version: '5.12',
     servers: [
       {
         id: 'default',
@@ -206,7 +206,7 @@ Accept();
 
   if (development === 'container') {
     const smtpd = {
-      version: '5.11',
+      version: '5.12',
       servers: [
         {
           id: 'default',
@@ -237,7 +237,7 @@ Accept();
     fs.writeFileSync(path.join(base, "src", "config", "smtpd.yaml"), yaml.stringify(smtpd));
     fs.writeFileSync(path.join(base, "dist", "smtpd.yaml"), yaml.stringify(smtpd));
     const rated = {
-      version: '5.11',
+      version: '5.12',
       environment: {
         controlsocket: {
           group: 'staff',
@@ -257,7 +257,7 @@ Accept();
     fs.writeFileSync(path.join(base, "src", "config", "rated.yaml"), yaml.stringify(rated));
     fs.writeFileSync(path.join(base, "dist", "rated.yaml"), yaml.stringify(rated));
     const dlpd = {
-      version: '5.11',
+      version: '5.12',
       environment: {
         controlsocket: {
           group: 'staff',
@@ -277,7 +277,7 @@ Accept();
     fs.writeFileSync(path.join(base, "src", "config", "dlpd.yaml"), yaml.stringify(dlpd));
     fs.writeFileSync(path.join(base, "dist", "dlpd.yaml"), yaml.stringify(dlpd));
     const dlpd_app = {
-      version: '5.11',
+      version: '5.12',
       rules:  []
     };
     fs.writeFileSync(path.join(base, "src", "config", "dlpd-app.yaml"), yaml.stringify(dlpd_app));
